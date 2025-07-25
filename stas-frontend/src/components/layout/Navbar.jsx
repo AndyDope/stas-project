7b6e30f4a9e33c9fa6729b7a59c94c6393df1047
Andy <anirudhsdoiphode@gmail.com>
2025-07-25 12:26:41 +0530

    Added: navbar and favicon icon changed

--- stas-frontend/src/components/layout/Navbar.jsx
+++ stas-frontend/src/components/layout/Navbar.jsx
@@ -1,58 +1,79 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Box, IconButton, Badge, Menu, MenuItem } from '@mui/material';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import React, { useState } from "react";
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	Box,
+	IconButton,
+	Badge,
+	Menu,
+	MenuItem,
+} from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+// import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleMenu = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  const handleLogout = () => {
-    // Add your logout logic from AuthContext here
-    handleClose();
-  };
+	const handleLogout = () => {
+		// Add your logout logic from AuthContext here
+		handleClose();
+	};
 
-  return (
-    <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-      <Toolbar>
-        <TaskAltIcon sx={{ mr: 1 }} />
-        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-          STAS - Mission Control
-        </Typography>
+	// Import the logo SVG
 
-        <Box>
-          {/* <IconButton size="large" color="inherit">
+	return (
+		<AppBar
+			position="fixed"
+			sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+		>
+			<Toolbar>
+				<Box
+					component="img"
+					src={logo}
+					alt="STAS Logo"
+					sx={{ height: 40, mr: 1 }}
+				/>
+
+				<Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+					Skill-based Task Allocation System
+				</Typography>
+
+				<Box>
+					{/* <IconButton size="large" color="inherit">
             <Badge badgeContent={4} color="error">
               <NotificationsIcon />
             </Badge>
           </IconButton> */}
-          <IconButton size="large" onClick={handleMenu} color="inherit">
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            sx={{ mt: '45px' }}
-          >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-          </Menu>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
+					<IconButton size="large" onClick={handleMenu} color="inherit">
+						<AccountCircle />
+					</IconButton>
+					<Menu
+						anchorEl={anchorEl}
+						open={open}
+						onClose={handleClose}
+						anchorOrigin={{ vertical: "top", horizontal: "right" }}
+						transformOrigin={{ vertical: "top", horizontal: "right" }}
+						sx={{ mt: "45px" }}
+					>
+						<MenuItem onClick={handleClose}>Profile</MenuItem>
+						<MenuItem onClick={handleLogout}>Logout</MenuItem>
+					</Menu>
+				</Box>
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 export default Navbar;
